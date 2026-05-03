d12789daee8604cefacfcf2ef5cef3dea5c1eff4
Deepansh Kesarwani <kesarwanideepansh@gmail.com>
2026-05-03 12:21:19 -0500

    byteorder.h: Added reversebytes and filie I/O wrappers

--- byteorder.h
+++ byteorder.h
@@ -1,7 +1,19 @@
 #ifndef BYTEORDER_H
 #define BYTEORDER_H
 
-// Returns a number other than 0 if the host CPU is big-endian, otherwise its 0.
+#include <cstdint>
+#include <cstdio>
+
+// Returns a number othan than 0 if the host CPU is big-endian, otherwise it returns 0.
 int isbigendian();
+
+// Reverses the byte order of a 64-bit unsigned integer.
+uint64_t reversebytes(uint64_t x);
+
+//writes one 8-byte unsigned integer in big-endian order to f. returns true on success, false on I/O error.
+bool writeuint64be(FILE* f, uint64_t v);
+
+//reads one 8 byte unsigned integer in big-endian order from f. Into v, Returns true if success, false if I/O error happens.
+bool readuint64_be(FILE* f, uint64_t& v);
 
 #endif
